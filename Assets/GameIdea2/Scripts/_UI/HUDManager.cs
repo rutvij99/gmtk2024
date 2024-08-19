1eb961077a160908c281bbc10c27b55cd2853f50
Rutvij <rutvijjoshi99@gmail.com>
2024-08-20 00:06:51 +0530

    HUD pass 2

--- Assets/GameIdea2/Scripts/_UI/HUDManager.cs
+++ Assets/GameIdea2/Scripts/_UI/HUDManager.cs
@@ -1,5 +1,6 @@
 using System;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
@@ -14,42 +15,80 @@ public class UploadMetaData
 
 public class HUDManager : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup editorView;
+    [SerializeField] private CanvasGroup simView;
+    [SerializeField] private CanvasGroup hud;
+    
+    [Header("hud stuff")]
     [SerializeField] private GameObject resetWorldButton;
     [SerializeField] private GameObject exitSimulationButton;
     [SerializeField] private CanvasGroup pauseMenu;
+    [SerializeField] private CanvasGroup controlsMenu;
+    
+    [Header("upload window")]
     [SerializeField] private CanvasGroup uploadWindow;
-
+    [SerializeField] private TMP_InputField authorInput;
+    [SerializeField] private TMP_InputField levelInput;
     private UploadMetaData uploadMeta = new UploadMetaData();
+
+    [Header("sim view stuff")]
+    [SerializeField] private CanvasGroup levelCompleteWindow;
+    [SerializeField] private RectTransform gallery;
+    [SerializeField] private GameObject galleryOpenIcon;
+    [SerializeField] private GameObject galleryCloseIcon;
+    
     
     [Space(10)]
+    [Header("Events")]
     public UnityEvent OnResetCameraClicked = new UnityEvent();
     public UnityEvent OnResetWorldClicked = new UnityEvent();
     public UnityEvent OnExitSimulationClicked = new UnityEvent();
     public UnityEvent OnExitToMainMenuClicked = new UnityEvent();
     public UnityEvent OnLoadNextLevelClicked = new UnityEvent();
+    public UnityEvent OnEnterSimulationClicked = new UnityEvent();
     public UnityEvent<string,string> OnUploadClicked = new UnityEvent<string,string>();
-
     
     public void EnableEditorView()
     {
         
+        resetWorldButton?.gameObject.SetActive(true);
+        exitSimulationButton?.gameObject.SetActive(false);
     }
 
     public void EnableSimView()
     {
         
-    }
-    
-    public void OnEnterEditor()
-    {
-        resetWorldButton?.gameObject.SetActive(true);
-        exitSimulationButton?.gameObject.SetActive(false);
-    }
-    
-    public void OnEnterSimulation()
-    {
         resetWorldButton?.gameObject.SetActive(false);
         exitSimulationButton?.gameObject.SetActive(true);
+    }
+
+    public void EnableLevelComplete()
+    {
+        levelCompleteWindow?.gameObject.SetActive(true);
+        // levelCompleteWindow.DOFade(1, 0.5f).OnComplete(() =>
+        // {
+        //     pauseMenu.gameObject.SetActive(false);
+        // });
+    }
+
+    public void ToggleGallery()
+    {
+        gallery.DOKill();
+        if (galleryOpenIcon.gameObject.activeInHierarchy)
+        {
+            // open gallery
+            galleryOpenIcon.gameObject.SetActive(false);
+            galleryCloseIcon.gameObject.SetActive(true);
+            gallery.DOSizeDelta(new Vector2(gallery.sizeDelta.x, 850), 0.5f);
+            
+        }
+        else
+        {
+            // close gallery
+            galleryOpenIcon.gameObject.SetActive(true);
+            galleryCloseIcon.gameObject.SetActive(false);
+            gallery.DOSizeDelta(new Vector2(gallery.sizeDelta.x, 60), 0.5f);
+        }
     }
     
     public void TogglePause()
@@ -96,7 +135,30 @@ public class HUDManager : MonoBehaviour
             });
         }
     }
+    
+    public void Button_ToggleControlsMenu()
+    {
+        if (controlsMenu.gameObject.activeInHierarchy)
+        {
+            // close
+            controlsMenu.DOFade(0, 0.1f).OnComplete(() =>
+            {           
+                controlsMenu.gameObject.SetActive(false);
+            });
+        }
+        else
+        {
+            // uploadMeta = new UploadMetaData();
+            // open
+            controlsMenu.gameObject.SetActive(true);
+            controlsMenu.alpha = 0;
+            controlsMenu.DOFade(1, 0.5f).OnComplete(() =>
+            {
 
+            });
+        }
+    }
+    
     public void Button_UploadConfirm()
     {
         OnUploadClicked?.Invoke(uploadMeta.leveName, uploadMeta.authorName);
@@ -129,6 +191,11 @@ public class HUDManager : MonoBehaviour
         GameConfig.Exit();
     }
 
+
+    public void Button_Simulate()
+    {
+        OnEnterSimulationClicked?.Invoke();
+    }
 
 
     public void Button_LoadNextLevel()

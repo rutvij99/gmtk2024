4c31a88e640b8ae57c2ffb53a0b30acaf1bb3ed8
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2024-08-19 03:23:09 -0400

    GassyPlanet added

---
+++ Assets/GameIdea2/Scripts/Planets/GassyPlanet.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace GameIdea2.Scripts.Planets
+{
+    public class GassyPlanet : MonoBehaviour
+    {
+        [SerializeField] private GameObject ExplosionFx;
+        [SerializeField] private GassyPlanetAsset DataAsset;
+
+        private Rigidbody rb;
+        
+        private void Start()
+        {
+            rb = GetComponentInChildren<Rigidbody>();
+            var mat = GetComponentInChildren<MeshRenderer>().material;
+            mat.SetColor("_PrimaryColor", DataAsset.PrimaryColors[Random.Range(0, DataAsset.PrimaryColors.Count)]);
+            mat.SetColor("_SecondaryColor", DataAsset.SecondaryColors[Random.Range(0, DataAsset.SecondaryColors.Count)]);
+            mat.SetFloat("_AtmosphereTurbulance", Mathf.Lerp(DataAsset.MinAtmThickness, DataAsset.MaxAtmThickness, Random.Range(0,1f)));
+        }
+
+        private void Update()
+        {
+            if (transform.localScale.x <= 0)
+            {
+                DestroyPlanet();
+                return;
+            }
+            rb.mass = Mathf.Lerp(DataAsset.MinMass, DataAsset.MaxMass, transform.localScale.x/ DataAsset.MaxScale);
+        }
+
+        private void DestroyPlanet()
+        {
+            var g = Instantiate(ExplosionFx, transform.position, Quaternion.identity);
+            Destroy(g.gameObject, 2);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/GameIdea2/Scripts/Planets/GassyPlanetAsset.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameIdea2.Scripts.Planets
+{
+    [CreateAssetMenu(menuName = "GMTK24/GassyPlanetAsset", fileName = "DefaultGassyPlanetAsset")]
+    public class GassyPlanetAsset : ScriptableObject
+    {
+        public List<Color> PrimaryColors;
+        public List<Color> SecondaryColors;
+        public float MinAtmThickness = 10;
+        public float MaxAtmThickness = 35;
+        public float MinMass = 75;
+        public float MaxMass = 175;
+        public float MaxScale = 75;
+    }
+}

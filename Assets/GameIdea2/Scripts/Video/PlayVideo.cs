939211a1a1c5e9282ba0dcdbe0c33cd0131a701e
Tanya Gupta <tanya1511gupta@gmail.com>
2024-08-20 16:13:49 +0530

    Added description and updated videos.

--- Assets/GameIdea2/Scripts/Video/PlayVideo.cs
+++ Assets/GameIdea2/Scripts/Video/PlayVideo.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -11,6 +12,8 @@ namespace GameIdea2.Video
         private const string SCALING = "Scaling_and_win";
         private const string MOVE_OBJECT = "move_object";
 
+        [SerializeField] private TextMeshProUGUI descriptionBox;
+
         private VideoPlayer videoPlayer;
 
         private void Start()
@@ -22,21 +25,37 @@ namespace GameIdea2.Video
         public void PlayPanVideo()
         {
             PlaySelectedVideo(PAN);
+            if (descriptionBox != null)
+            {
+                descriptionBox.text = "Hold middle mouse button to scroll.";
+            }
         }
 
         public void PlayZoomInOutVideo()
         {
             PlaySelectedVideo(ZOOM_IN_OUT);
+            if (descriptionBox != null)
+            {
+                descriptionBox.text = "Scroll up and down to zoom in and out.";
+            }
         }
         
         public void PlayScalingVideo()
         {
             PlaySelectedVideo(SCALING);
+            if (descriptionBox != null)
+            {
+                descriptionBox.text = "Hold right mouse button and drag to scale up or down.";
+            }
         }
         
         public void PlayMoveObjectVideo()
         {
             PlaySelectedVideo(MOVE_OBJECT);
+            if (descriptionBox != null)
+            {
+                descriptionBox.text = "Hold left mouse button to move and reach the target location to win.";
+            }
         }
         
         private void PlaySelectedVideo(string videoName)
@@ -45,6 +64,7 @@ namespace GameIdea2.Video
             videoPlayer?.Stop();
             if (videoPlayer == null) videoPlayer = GetComponent<VideoPlayer>();
             videoPlayer.clip = Resources.Load<VideoClip>(videoName);
+            
             videoPlayer?.Play();
         }
     }

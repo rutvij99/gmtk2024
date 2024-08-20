9501384e5c8773868646bf918ba19f9482a20d45
Tanya Gupta <tanya1511gupta@gmail.com>
2024-08-20 14:40:09 +0530

    Added video tutorials

---
+++ Assets/GameIdea2/Scripts/Video/PlayVideo.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace GameIdea2.Video
+{
+    public class PlayVideo : MonoBehaviour
+    {
+        private const string PAN = "pan_tut";
+        private const string ZOOM_IN_OUT = "zoom_in_out";
+        private const string SCALING = "Scaling_and_win";
+        private const string MOVE_OBJECT = "move_object";
+
+        private VideoPlayer videoPlayer;
+
+        private void Start()
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+            PlayPanVideo();
+        }
+
+        public void PlayPanVideo()
+        {
+            PlaySelectedVideo(PAN);
+        }
+
+        public void PlayZoomInOutVideo()
+        {
+            PlaySelectedVideo(ZOOM_IN_OUT);
+        }
+        
+        public void PlayScalingVideo()
+        {
+            PlaySelectedVideo(SCALING);
+        }
+        
+        public void PlayMoveObjectVideo()
+        {
+            PlaySelectedVideo(MOVE_OBJECT);
+        }
+        
+        private void PlaySelectedVideo(string videoName)
+        {
+            videoPlayer?.Stop();
+            videoPlayer.clip = Resources.Load<VideoClip>(videoName);
+            videoPlayer?.Play();
+        }
+    }
+}
+
